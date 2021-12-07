dbfefd16e5b3178e2121606455aa2a4031374943
Adrian Kiva <akiva@mta.ca>
2021-12-07 07:51:58 -0500

    add body parser and handle post requests to url/new

--- express_server.js
+++ express_server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
@@ -32,6 +36,11 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
