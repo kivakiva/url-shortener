edc40dea88fc05d76aad925a29b2da511638f6fd
Adrian Kiva <akiva@mta.ca>
2021-12-08 21:16:34 -0500

    install cookie parser and import

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
